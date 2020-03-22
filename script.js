6bc5175f2689973626c3173a3eb5a25d0479a135
Kevin Smeraglio <ksmera01@gmail.com>
2020-03-22 19:58:50 -0400

    updated html and js

--- script.js
+++ script.js
@@ -6,15 +6,12 @@ var startButton = document.getElementById("startButton");
 // // Selecting the Question Options
 var questionSection = document.getElementById("questionOptions");
 // // Selecting the Button Options
-// var buttonSection = document.getElementById("buttonOptions");
-// var questionsArray = ["What number did Phillies player Chase Utley wear?", "Who was the Phillies manager during the 2008 World Series?", "What is the name of the Phillies stadium?", "How many Super Bowl Championships have the Philadelphia Eagles won?", "In what year did the Philadelphia Flyers last win a Stanley Cup?"]
+var buttonSection = document.getElementById("buttonOptions");
+var questionsArray = ["What number did Phillies player Chase Utley wear?", "Who was the Phillies manager during the 2008 World Series?", "What is the name of the Phillies stadium?", "How many Super Bowl Championships have the Philadelphia Eagles won?", "In what year did the Philadelphia Flyers last win a Stanley Cup?"]
 // var answerArray = [26, 32, 11, 6];
+var showInitialsForm = document.getElementById("form");
 var score = secondsLeft - secondsElapsed;
-var secondsElapsed = 0
-
-// var timeEl = document.querySelector(".time");
-// var mainEl = document.getElementById("main");
-
+var secondsElapsed = 0;
 var secondsLeft = 75;
 
 function setTime() {
@@ -33,34 +30,25 @@ function setTime() {
 
 function sendMessage() {
     questionSection.textContent = "All Done. Your Score is: " + score;
+    buttonSection.setAttribute("class", "d-block form-group");
+}
+
+function displayQuestion() {
+    questionSection.textContent = questionsArray[0];
+}
+
+function displayUserOptions() {
+
 }
 
 startButton.addEventListener("click", myFunction);
 function myFunction() {
-    setTime()
-    if (secondsLeft === 0) {
-        clearInterval(timerInterval);
+    setTime();
+    document.getElementById("startButton").style.visibility = 'hidden';
+    displayQuestion();
 
-    }
 }
-// function nextQuestion() {
-//     for (let i = 0; i < questionsArray.length; i++) {
-//         var followUpQuestion = document.querySelector("p");
-//         followUpQuestion.textContent = questionsArray[i];
-//     }
 
-// for (var j = 0; j < favTVshows.length; j++) {
 
-//     // ...and create alert messages listing out each show in our array.
-//     alert(favTVshows[j]);
-// }
-// var followUpQuestion = document.querySelector("p");
-//     // followUpQuestion.textContent = questionsArray[i];
-// }
 
-    //   mainEl.appendChild(imgEl);
-
-    // }
-
-    // setTime();
 
